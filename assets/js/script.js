a51057b057856ae6ebc7222a6b95015d55ceb88b
Jacastro619 <jorgecastro619@gmail.com>
2023-10-05 17:36:58 -0700

    add jquery link and begin JavaScript file with variables and selectors

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+var apiKey;
+var city;
+var searchBtn = $("#search-btn");
+var historyBtn = $("#history-btn");
+var searchInput = $("#search-input");
+var formEl = $("#search-form");
+var weatherContent = $("#weather-content");
+
+var testUrl =
+  "api.openweathermap.org/data/2.5/forecast?q=san diego&appid=a55b9587095f1ce63827f16c4ab8c6ef";
