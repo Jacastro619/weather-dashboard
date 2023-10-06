dbeb359d40c09589cc579e95a0eb27775aadcefa
Jacastro619 <jorgecastro619@gmail.com>
2023-10-05 23:31:38 -0700

    Add api functionality to autopopulate html file with data from api call

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var apiKey = "a55b9587095f1ce63827f16c4ab8c6ef";
-var city = "fresno";
+var city = "salem";
 var searchBtn = $("#search-btn");
 var historyBtn = $("#history-btn");
 var searchInput = $("#search-input");
@@ -8,9 +8,10 @@ var weatherContent = $("#weather-content");
 var currDayCity = $("#cur-day-city");
 var date = dayjs().format("M/D/YYYY");
 var currDayDate;
+var numArray = ["1", "2", "3", "4", "5"];
 
-var currDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
-var forecastUrl = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`;
+var currDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${apiKey}&units=imperial`;
+var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&appid=${apiKey}&units=imperial`;
 console.log(date);
 function getCurrentDayApi() {
   fetch(currDayUrl)
@@ -41,17 +42,42 @@ function getForecastApi() {
     })
     .then(function (data) {
       console.log(data);
+      var dataArr = [
+        data.list[0],
+        data.list[8],
+        data.list[16],
+        data.list[24],
+        data.list[32],
+      ];
       var icon;
-      if (data.weather[0].main === "Clear") {
-        icon = "☀";
-      } else if (data.weather[0].main === "Clouds") {
-        icon = "☁";
-      } else {
-        icon = "🌧";
+      for (var i = 0; i < dataArr.length; i++) {
+        if (dataArr[i].weather[0].main === "Clear") {
+          icon = "☀";
+        } else if (dataArr[i].weather[0].main === "Clouds") {
+          icon = "☁";
+        } else {
+          icon = "🌧";
+        }
+
+        var date = dataArr[i].dt_txt.split("").splice(0, 10).join("");
+        var imperialDate = dayjs(date).format("M/D/YYYY");
+
+        $(`#fut-date${numArray[i]}`).text(imperialDate);
+        $(`#fut-icon${numArray[i]}`).text(icon);
+        $(`#fut-temp${numArray[i]}`).text(
+          `Temp: ${Math.floor(dataArr[i].main.temp)} °F `
+        );
+        $(`#fut-wind${numArray[i]}`).text(
+          `Wind: ${(dataArr[i].wind.speed * 2.237).toFixed(2)} MPH`
+        );
+        $(`#fut-hum${numArray[i]}`).text(
+          `Humidity: ${dataArr[i].main.humidity}%`
+        );
       }
     });
 }
 
+getForecastApi();
 getCurrentDayApi();
 
 //Clear, Clouds, Rain
