3cecf76865e99372fcae7c7e2f42150cddf4cdc0
Jacastro619 <jorgecastro619@gmail.com>
2023-10-05 20:16:41 -0700

    add current day display on html file with future day cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,4 +7,21 @@ var formEl = $("#search-form");
 var weatherContent = $("#weather-content");
 
 var testUrl =
-  "api.openweathermap.org/data/2.5/forecast?q=san diego&appid=a55b9587095f1ce63827f16c4ab8c6ef";
+  "https://api.openweathermap.org/data/2.5/forecast?q=san%20jose&appid=a55b9587095f1ce63827f16c4ab8c6ef&units=imperial&cnt=1";
+
+function getApi() {
+  fetch(testUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      for (var i = 0; i < data.list.length; i++) {
+        var temp = Math.floor(data.list[0].main.temp);
+        var humidity = data.list[0].main.humidity;
+        console.log(temp, humidity);
+      }
+    });
+}
+
+getApi();
