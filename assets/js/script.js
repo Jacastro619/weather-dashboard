011decd24c643195f82c7f1218a0f6cc47e6cd99
Jacastro619 <jorgecastro619@gmail.com>
2023-10-06 19:11:46 -0700

    add functionality to history buttons. Now they call the api when clicked. Add script to prevent duplicate buttons if a city was searched more than once

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,45 +32,29 @@ function renderBtns() {
   }
 }
 
-function getForecastApi(event) {
-  event.preventDefault();
-  city = searchInput.val().trim();
+function histBtnForecast(event) {
+  city = $(event.target).text();
+
   var currDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${apiKey}&units=imperial`;
   var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&appid=${apiKey}&units=imperial`;
-  console.log(city);
+
   fetch(currDayUrl)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      console.log(data);
-      if (data.cod !== 200) {
-        alert("Invalid input, please check spelling.");
+      var icon;
+      if (data.weather[0].main === "Clear") {
+        icon = "☀";
+      } else if (data.weather[0].main === "Clouds") {
+        icon = "☁";
       } else {
-        var btnEl = $("<button>");
-        btnEl.attr({
-          id: "history-btn",
-          type: "button",
-          class: "btn btn-info w-100 custom-btn",
-        });
-        btnEl.text(city);
-        prevSearchEl.append(btnEl);
-        historyArray.push(city);
-        console.log(historyArray);
-        localStorage.setItem("historyArr", JSON.stringify(historyArray));
-        var icon;
-        if (data.weather[0].main === "Clear") {
-          icon = "☀";
-        } else if (data.weather[0].main === "Clouds") {
-          icon = "☁";
-        } else {
-          icon = "🌧";
-        }
-        currDayCity.text(`${data.name} ${date} ${icon}`);
-        $("#cur-day-temp").text(`Temp: ${Math.floor(data.main.temp)}`);
-        $("#cur-day-wind").text(`Wind: ${data.wind.speed.toFixed(2)} MPH`);
-        $("#cur-day-humidity").text(`Humidity: ${data.main.humidity}%`);
+        icon = "🌧";
       }
+      currDayCity.text(`${data.name} ${date} ${icon}`);
+      $("#cur-day-temp").text(`Temp: ${Math.floor(data.main.temp)}`);
+      $("#cur-day-wind").text(`Wind: ${data.wind.speed.toFixed(2)} MPH`);
+      $("#cur-day-humidity").text(`Humidity: ${data.main.humidity}%`);
     });
 
   fetch(forecastUrl)
@@ -114,5 +98,92 @@ function getForecastApi(event) {
     });
 }
 
+function getForecastApi(event) {
+  event.preventDefault();
+  city = searchInput.val().trim();
+  var currDayUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},us&appid=${apiKey}&units=imperial`;
+  var forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city},us&appid=${apiKey}&units=imperial`;
+  console.log(city);
+  if (city !== "") {
+    fetch(currDayUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        if (data.cod !== 200) {
+          alert("Invalid input, please check spelling.");
+        } else {
+          if (historyArray.includes(city)) {
+          } else {
+            var btnEl = $("<button>");
+            btnEl.attr({
+              id: "history-btn",
+              type: "button",
+              class: "btn btn-info w-100 custom-btn",
+            });
+            btnEl.text(city);
+            prevSearchEl.append(btnEl);
+            historyArray.push(city);
+          }
+          localStorage.setItem("historyArr", JSON.stringify(historyArray));
+          var icon;
+          if (data.weather[0].main === "Clear") {
+            icon = "☀";
+          } else if (data.weather[0].main === "Clouds") {
+            icon = "☁";
+          } else {
+            icon = "🌧";
+          }
+          currDayCity.text(`${data.name} ${date} ${icon}`);
+          $("#cur-day-temp").text(`Temp: ${Math.floor(data.main.temp)}`);
+          $("#cur-day-wind").text(`Wind: ${data.wind.speed.toFixed(2)} MPH`);
+          $("#cur-day-humidity").text(`Humidity: ${data.main.humidity}%`);
+        }
+      });
+
+    fetch(forecastUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        var dataArr = [
+          data.list[0],
+          data.list[8],
+          data.list[16],
+          data.list[24],
+          data.list[32],
+        ];
+        var icon;
+        for (var i = 0; i < dataArr.length; i++) {
+          if (dataArr[i].weather[0].main === "Clear") {
+            icon = "☀";
+          } else if (dataArr[i].weather[0].main === "Clouds") {
+            icon = "☁";
+          } else {
+            icon = "🌧";
+          }
+
+          var date = dataArr[i].dt_txt.split("").splice(0, 10).join("");
+          var imperialDate = dayjs(date).format("M/D/YYYY");
+
+          $(`#fut-date${numArray[i]}`).text(imperialDate);
+          $(`#fut-icon${numArray[i]}`).text(icon);
+          $(`#fut-temp${numArray[i]}`).text(
+            `Temp: ${Math.floor(dataArr[i].main.temp)} °F `
+          );
+          $(`#fut-wind${numArray[i]}`).text(
+            `Wind: ${(dataArr[i].wind.speed * 2.237).toFixed(2)} MPH`
+          );
+          $(`#fut-hum${numArray[i]}`).text(
+            `Humidity: ${dataArr[i].main.humidity}%`
+          );
+        }
+      });
+  } else alert("Must enter a valid city");
+}
+
 formEl.on("submit", getForecastApi);
+$(document).on("click", "#history-btn", histBtnForecast);
 //Clear, Clouds, Rain
